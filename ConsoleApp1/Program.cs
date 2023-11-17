6bb0f8ecea019e596864bf02f9cf1395534b89ce
maarrrrrrii <masho.pup@gmail.com>
2023-11-17 09:26:39 +0300

    поздоровалась с Камиллой

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Привет, мир!");
+            Console.WriteLine("Привет, Камилла!");
         }
     }
 }
